7c186db917fe44824dc2e2905acd2109575f95e3
butterbeetle <butterbeetle@naver.com>
2023-04-13 23:10:36 +0900

    404페이지 추가

--- src/pages/ErrorPage.js
+++ src/pages/ErrorPage.js
@@ -1,5 +1,14 @@
+import { Link } from "react-router-dom";
+import styles from "./ErrorPage.module.css";
+
 const ErrorPage = () => {
-  return <div>에러입니다.</div>;
+  return (
+    <div className={styles["error"]}>
+      <span className={styles["status"]}>404</span>
+      <span className={styles["message"]}>존재하지 않는 페이지 입니다.</span>
+      <Link to="/signin">홈으로</Link>
+    </div>
+  );
 };
 
 export default ErrorPage;
